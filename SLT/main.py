56ecef0023a0494e5eba87e70aa1135a4bdef449
Chanakya Devendra <101084514+mrdev-19@users.noreply.github.com>
2024-03-18 12:22:23 +0530

    Update main.py

--- SLT/main.py
+++ SLT/main.py
@@ -133,13 +133,13 @@ def main():
     )    
     if(opt=="Live Video Input"):
         run = st.checkbox('Run')
-        FRAME_WINDOW = st.image([])
-        camera = cv2.VideoCapture(0)
+        # FRAME_WINDOW = st.image([])
+        # camera = cv2.VideoCapture(0)
 
-        while run:
-            _, frame = camera.read()
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            FRAME_WINDOW.image(frame)
+        # while run:
+        #     _, frame = camera.read()
+        #     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        #     FRAME_WINDOW.image(frame)
             
         else:
             st.write('Stopped')
